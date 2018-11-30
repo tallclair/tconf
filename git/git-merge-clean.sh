e38e0957f451f2773ebc2d05d44217ea243406b0
Tim Allclair <tallclair@google.com>
2018-11-29 19:22:39 -0800

    misc workstation sync

--- git/git-merge-clean.sh
+++ git/git-merge-clean.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-set -euf -o pipefail
+set -euf
 
-git branch --merged | grep -v '\*' | grep -v master | grep -v dev | xargs -n 1 git branch -d
+UPSTREAM="${UPSTREAM:-upstream}"
+WORKING_BRANCH="$(git current-branch)"
+
+git fetch "${UPSTREAM}" master
+git checkout -B master "${UPSTREAM}/master"
+git branch --merged "${UPSTREAM}/master" | grep -v '\*' | grep -v master | grep -v dev | xargs -r -n1 git branch -d
+
+# Special case cherry-picks
+for rel in $(git branch -r -l "${UPSTREAM}/release-*" | grep -Ev "/release-(0.*|1.[0123]$)"); do
+  git checkout -B $(basename "$rel") "$rel"
+  git branch --merged "$rel" | grep -v '\*' | grep -v $(basename "$rel") || true
+done
+
+git checkout "$WORKING_BRANCH"
