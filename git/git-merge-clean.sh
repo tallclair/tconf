90005f57db5e78bf88bb5dcb48901ba2bf3243c6
Tim Allclair <tallclair@google.com>
2020-08-03 16:15:49 -0700

    Fix merge-clean script

--- git/git-merge-clean.sh
+++ git/git-merge-clean.sh
@@ -7,13 +7,17 @@ WORKING_BRANCH="$(git current-branch)"
 
 git fetch "${UPSTREAM}" master
 git checkout -B master "${UPSTREAM}/master"
+sleep 1
+
 git branch --merged "${UPSTREAM}/master" | grep -v '\*' | grep -v master | grep -v dev | xargs -r -n1 git branch -d
+sleep 1
 
 # Special case cherry-picks
-for rel in $(git branch -r -l "${UPSTREAM}/release-*" | grep -Ev "/release-(0.*|1.[0123]$)"); do
+for rel in $(git branch -r -l "${UPSTREAM}/release-*" | grep -E "/release-1.[1-9][0-9]$"); do
   git checkout -B $(basename "$rel") "$rel"
   sleep 1
-  git branch --merged "$rel" | grep -v '\*' | grep -v $(basename "$rel") || true
+  git branch --merged "$rel" | grep -v '\*' | grep -v $(basename "$rel") | xargs -r -n1 git branch -d
+  sleep 1
 done
 
 git checkout "$WORKING_BRANCH"
