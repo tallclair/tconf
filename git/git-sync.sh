7e590b0fe0a4fc5043452201d01f12ad5d4978b8
Tim Allclair <timallclair@gmail.com>
2021-04-02 12:03:57 -0700

    git-sync support for main branch

--- git/git-sync.sh
+++ git/git-sync.sh
@@ -1,7 +1,19 @@
 #!/bin/sh -e
 
 REMOTE=${1:-upstream}
-BRANCH=${2:-master}
+BRANCH=${2:-}
 
 git fetch $REMOTE
+
+if [[ -z "$BRANCH" ]]; then
+    if [[ ! -z "$(git branch -r --list ${REMOTE}/master)" ]]; then
+        BRANCH=master
+    elif [[ ! -z "$(git branch -r --list ${REMOTE}/main)" ]]; then
+        BRANCH=main
+    else
+        >&2 echo "cannot determine default branch"
+        exit 1
+    fi
+fi
+
 git rebase $REMOTE/$BRANCH
