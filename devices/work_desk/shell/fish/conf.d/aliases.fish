6e5fdfb87c2dede59b47e8500f5d9be6adc3529f
Tim Allclair <tallclair@google.com>
2026-03-12 18:39:01 +0000

    ignore flag for go-test (and other misc)

--- devices/work_desk/shell/fish/conf.d/aliases.fish
+++ devices/work_desk/shell/fish/conf.d/aliases.fish
@@ -3,9 +3,9 @@ abbr -a k8s-e2e 'kubetest -v'
 abbr -a k8s-run-e2e 'make WHAT=test/e2e/e2e.test && KUBERNETES_CONFORMANCE_TEST=y hack/ginkgo-e2e.sh --ginkgo.focus'
 abbr -a k8s-make-e2e 'bazel build //test/e2e:e2e.test_binary //vendor/github.com/onsi/ginkgo/ginkgo:ginkgo'
 
-abbr -a kind-test-e2e 'make WHAT="test/e2e/e2e.test" && ./_output/bin/e2e.test -context kind-kind -ginkgo.focus="$FOCUS" -num-nodes 2'
-abbr -a kind-test-e2e-rerun './_output/bin/e2e.test -context kind-kind -ginkgo.focus="$FOCUS" -num-nodes 2'
+abbr -a kind-test-e2e 'ginkgo --focus="$FOCUS" ./test/e2e -- -context kind-kind -num-nodes 2'
 abbr -a kind-create-e2e-cluster 'kind create cluster --config $HOME/src/github.com/tallclair/k8s-devel/config/kind-e2e-config.yaml --image kindest/node:latest'
+abbr -a kind-rebuild-e2e-cluster 'kind delete cluster && kind build node-image && kind create cluster --config $HOME/src/github.com/tallclair/k8s-devel/config/kind-e2e-config.yaml --image kindest/node:latest'
 
 abbr -a k-use-ns 'kubectl config set-context --current --namespace'
 
